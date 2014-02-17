54c98b66a9d0133d49df2c07b3f154753abb2e5e
Severin Schols <severin@schols.de>
2014-02-17 21:39:49 +0100

    better index page

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,10 +1,22 @@
 <?php
 $status = substr(file_get_contents("../current_status"),0,1);
-if ($status == "1" ) {
-	echo "The Munich Maker Lab is currently open.";
-} elseif  ($status == "0") {
-	echo "The Munich Maker Lab is currently closed.";
-} else {
-	echo "unknown";
-}
 ?>
+<html>
+	<head>
+		<title>Munich Maker Lab - Lab Status</title>
+	</head>
+	<body>
+		<h1>Munich Maker Lab - Lab Status</h1>
+		<?php if ($status == "1" ) { ?>
+			<img src="open.png">
+			<p>The lab is currently open.<br>If you don't have a key, call 089-21553954.</p>
+		<?php } elseif  ($status == "0") { ?>
+			<img src="closed.png">
+			<p>The lab is currently closed.</p>
+		<?php } else { ?>
+			<img src="unknown.png">
+			<p>The status of the lab is currently unknown.</p>
+		<?php } ?>
+	</body>
+</html>
+
