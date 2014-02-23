fda4e9972cafb718e95ff1aa27604760fb82218a
Severin Schols <severin@schols.de>
2014-02-23 19:16:59 +0100

    switched mqtt client implementation, and added better mqtt connection handling

--- daemon.py
+++ daemon.py
@@ -1,24 +1,54 @@
-import mosquitto
+import paho.mqtt.client as paho
+from threading import Timer
+
 import config
 
 def on_connect(mosq, obj, rc):
+	print "[MQTT] Connect with RC " + str(rc)
 	mosq.subscribe(config.topic, 0)
 	# print("rc: "+str(rc))
 
 def on_message(mosq, obj, msg):
-	# print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+	print "[MQTT] " + msg.topic + " [" + str(msg.qos) + "]: " + str(msg.payload)
 	f = open("current_status","w")
 	f.write(msg.payload)
 	f.close()
 
 def on_subscribe(mosq, obj, mid, granted_qos):
-	print("Subscribed: "+str(mid)+" "+str(granted_qos))
+	print "[MQTT] Subscribed: "+str(mid)+" "+str(granted_qos)
 
-mqttc = mosquitto.Mosquitto("mumalab_status_daemon_test")
+def on_disconnect(client, userdata, rc):
+	print "[MQTT] Disconnected " + str(rc)
+	try_reconnect(client)
+
+def on_log(client, userdata, level, buf):
+	print "[MQTT] LOG: " + buf
+	
+def try_reconnect(client, time = 60):
+	try:
+		print "[MQTT] Trying reconnect"
+		client.reconnect()
+	except:
+		print "[MQTT] Reconnect failed. Trying again in " + str(time) + " seconds"
+		Timer(time, try_reconnect, [client]).start()
+
+
+print "[Main] Initializing MQTT"
+mqttc = paho.Client()
 mqttc.username_pw_set(config.broker["user"], config.broker["password"])
 mqttc.on_message = on_message
 mqttc.on_connect = on_connect
-# mqttc.on_subscribe = on_subscribe
-mqttc.connect(config.broker["hostname"], config.broker["port"], 60)
+mqttc.on_disconnect = on_disconnect
+mqttc.on_subscribe = on_subscribe
+mqttc.on_log = on_log
+try:
+	mqttc.connect(config.broker["hostname"], config.broker["port"])
+except:
+	print "[MQTT] Connection failed. Trying again in 30 seconds"
+	Timer(30, try_reconnect, [mqttc]).start()
 
+print "[Main] Entering loop"
 mqttc.loop_forever()
+
+print "[Main] Exiting"
+mqttc.disconnect()
--- rpi.py
+++ rpi.py
@@ -1,31 +1,52 @@
 #/bin/python
 import RPi.GPIO as GPIO
-import mosquitto
+import paho.mqtt.client as paho
+
 from threading import Timer
 
 room_open = False
 
+def on_disconnect(client, userdata, rc):
+	print "[MQTT] Disconnected " + str(rc)
+	try_reconnect(client)
+
+def on_log(client, userdata, level, buf):
+	print "[MQTT] LOG: " + buf
+
+# MQTT reconnect
+def try_reconnect(client, time = 60):
+	try:
+		print "[MQTT] Trying reconnect"
+		client.reconnect()
+	except:
+		print "[MQTT] Reconnect failed. Trying again in " + str(time) + " seconds"
+		Timer(time, try_reconnect, [client]).start()
+
+
 # post the status to MQTT and the local console
 def print_status():
+	print "[MQTT] Publishing status"
 	if room_open:
-		print "Current status: open."
-		mqtt_client.publish(config.topic, "1", 1, True)
+		mqttc.publish(config.topic, "1", 1, True)
 	else:
 		print "Current status: closed."
-		mqtt_client.publish(config.topic, "0", 1, True)
+		mqttc.publish(config.topic, "0", 1, True)
 
 # after timer has fired, get the status
 def set_status(channel):
 	global room_open
+	print "[GPIO] Getting status"
 	if GPIO.input(channel) == GPIO.HIGH:
 		room_open = True
 	else:
 		room_open = False
+	print "[GPIO] Current Status: " + str(room_open)
 	print_status()
 
 # Reset timer when interrupt is received
 def status_callback(channel):
 	global bounce_timer
+	print "[GPIO] interrupt detected"
 	bounce_timer.cancel()
 	bounce_timer = Timer(2.0, set_status, [channel])
 	bounce_timer.start()
@@ -38,21 +59,17 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 # initialize MQTT
-mqtt_client = mosquitto.Mosquitto("mumalab_status_box")
-mqtt_client.username_pw_set(config.broker["user"], config.broker["password"])
-mqtt_client.will_set(config.topic, "?", 1, True)
-mqtt_client.connect(config.broker["hostname"], config.broker["port"], 60)
+mqttc = paho.client("mumalab_status_box")
+mqttc.username_pw_set(config.broker["user"], config.broker["password"])
+mqttc.will_set(config.topic, "?", 1, True)
+mqttc.connect(config.broker["hostname"], config.broker["port"], 60)
 
 # add interrupt to GPIO pin
 GPIO.add_event_detect(23, GPIO.BOTH, callback=status_callback)
 
 # Loop forever
-try:
-	while 1:
-		mqtt_client.loop()
-except KeyboardInterrupt:
-	pass
+mqttc.loop_forever()
 
 # Clean up afterwards
 GPIO.cleanup()
-mqtt_client.disconnect()
+mqttc.disconnect()
