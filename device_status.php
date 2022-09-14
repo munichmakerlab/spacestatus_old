47189946d499a2c0c06ac6de86cb519f2e849832
Christoph Maser <christoph.maser@1und1.de>
2022-09-14 13:02:49 +0200

    fix: parsing of project and ThingInfoBox

--- device_status.php
+++ device_status.php
@@ -2,16 +2,39 @@
 
 function getDeviceStatus($device) {
 	    $url = "https://wiki.munichmakerlab.de/index.php?title=" . $device . "&action=edit";
-	        $raw = file_get_contents($url);
-	        preg_match('/status\s+=\s+(\S+)/', $raw, $matches);
-//		echo $device . ": " .  $matches[1] . "\n";
-		return $matches[1];
+	    $raw = file_get_contents($url);
+	    preg_match_all('/\{\{(ThingInfoBox|project)(.+?)\}\}/s', $raw, $matches);
+		$result = array();
+		foreach ($matches[2] as $entry) {
+			foreach ((explode(PHP_EOL, $entry)) as $line) {
+				if(!str_contains($line, "=")) {
+					continue;
+				}
+				$line = trim($line);
+				$line = str_replace("|", "", $line);
+				$foo = explode("=", $line, 2);
+				$res[trim($foo[0])] = trim($foo[1]);
+			}
+
+			array_push($result, $res);
+		}
+
+		#echo $device . ": " .  $matches[1] . "\n";
+		return $result;
+	}
+
+$data = array();
+$names = array(
+	"Lusa" => "Lusa (3D printer)",
+	"Rusa" => "Rusa (3D printer)",
+	"LaserCutter" => "Laser Cutter",
+	"CNC Mill" => "CNC Mill",
+);
+foreach (['Prusa_Mini','LaserCutter', 'CNC_router_build'] as $device) {
+	foreach (getDeviceStatus($device) as $entry) {
+		$data[$names[$entry['name']]] =  $entry["status"];
+	}
 }
 
-file_put_contents(__DIR__ . '/devices.json',  json_encode(array(
-	"Lusa (3D printer)" => getDeviceStatus("Lusa"),
-	"Rusa (3D printer)" => getDeviceStatus("Rusa"),
-	"Lasercutter" => getDeviceStatus("LaserCutter"),
-	"CNC Mill"=> getDeviceStatus("CNC_router_build")
-)));
-?>
+file_put_contents(__DIR__ . '/devices.json',  json_encode($data));
+
